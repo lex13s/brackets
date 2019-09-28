727b8defe59a920aa164ac5f13a0914d53370dc8
Alexey Seleznev <jobmy@mail.ru>
2019-09-28 17:23:42 +0300

    feat: create function for string

--- src/index.js
+++ src/index.js
@@ -1,37 +1,67 @@
-// module.exports = function check(str, bracketsConfig) {
-//   // your solution
-// }
-let str = "((()))()";
+module.exports = function check(str, bracketsConfig) {
 
-function check(str, bracketsConfig) {
     let result = [];
     let arr    = str.split("");
 
-    if (arr[0] === ")"  //Проверим, что аргумент НЕ начинается с закрывающейся скобки
+    if (arr[0] === ")"
         || arr[0] === "}"
         || arr[0] === "]") {
         console.log(false);
         return false;
     }
     result.push(arr[0]);
-    console.log(result);
 
     for (let i = 1; i < arr.length; i++){
-        result.push(arr[i]);
+        let j = 0;
 
-        //console.log(result);
+        if (result[j] !== arr[i]) { //если пара совпала, удаляем из стека
+            result.shift();
+        } else {
+            result.push(arr[i]); //если пара не совпала - добавляем в стек
+            j++;
+        }
     }
-    // arr.forEach((e, i) => {
-    //     e++;
-    //
-    //     result.push(e);
-    //
-    //     console.log(result);
-    //
-    //     //result.shift(e);
-    // });
-    console.log(result);
-    if(result[0] === undefined) return true;
-}
 
-check(str);
+    if (result[0] === undefined) {
+        return true;
+    } else {
+        return false
+    }
+};
+
+// let str = "(({})(({}))([])({})[])";
+//
+// function check(str, bracketsConfig) {
+//     let result = [];
+//     let arr    = str.split("");
+//
+//     if (arr[0] === ")"  //Проверим, что аргумент НЕ начинается с закрывающейся скобки
+//         || arr[0] === "}"
+//         || arr[0] === "]") {
+//         console.log(false);
+//         return false;
+//     }
+//
+//     result.push(arr[0]);
+//
+//     for (let i = 1; i < arr.length; i++){
+//         let j = 0;
+//
+//         if (result.length >= 1 && result[j] !== arr[i]) { //если пара совпала, удаляем из стека
+//             console.log(result[j], arr[i]);
+//             result.shift();
+//         } else {
+//             result.push(arr[i]); //если пара не совпала - добавляем в стек
+//             j++;
+//             console.log(result[j], arr[i]);
+//         }
+//
+//     }
+//     console.log(result);
+//     if (result[0] === undefined) {
+//         return true;
+//     } else {
+//         return false
+//     }
+// }
+// console.log(check(str));
