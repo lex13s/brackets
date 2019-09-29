71fab55a125ecd6654d71b42da79899886903bf1
Alexey Seleznev <jobmy@mail.ru>
2019-09-29 12:39:15 +0300

    need refactor func #2

--- src/index.js
+++ src/index.js
@@ -1,34 +1,34 @@
-module.exports = function check(str, bracketsConfig) {
-
-    let result = [];
-    let arr    = str.split("");
-
-    if (arr[0] === ")"
-        || arr[0] === "}"
-        || arr[0] === "]") {
-        console.log(false);
-        return false;
-    }
-    result.push(arr[0]);
-
-    for (let i = 1; i < arr.length; i++){
-        let j = 0;
-
-        if (result[j] !== arr[i]) { //если пара совпала, удаляем из стека
-            result.shift();
-        } else {
-            result.push(arr[i]); //если пара не совпала - добавляем в стек
-            j++;
-        }
-    }
-
-    if (result[0] === undefined) {
-        return true;
-    } else {
-        return false
-    }
-};
-
+// module.exports = function check(str, bracketsConfig) {
+//
+//     let result = [];
+//     let arr    = str.split("");
+//
+//     if (arr[0] === ")"
+//         || arr[0] === "}"
+//         || arr[0] === "]") {
+//         console.log(false);
+//         return false;
+//     }
+//     result.push(arr[0]);
+//
+//     for (let i = 1; i < arr.length; i++){
+//         let j = 0;
+//
+//         if (result[j] !== arr[i]) { //если пара совпала, удаляем из стека
+//             result.shift();
+//         } else {
+//             result.push(arr[i]); //если пара не совпала - добавляем в стек
+//             j++;
+//         }
+//     }
+//
+//     if (result[0] === undefined) {
+//         return true;
+//     } else {
+//         return false
+//     }
+// };
+//======temp=========
 // let str = "(({})(({}))([])({})[])";
 //
 // function check(str, bracketsConfig) {
@@ -64,4 +64,89 @@ module.exports = function check(str, bracketsConfig) {
 //         return false
 //     }
 // }
+// console.log(check(str));
+
+
+//==========для многомерного массива===============
+
+let bracketsConfig = [['(', ')'], ['[', ']', ")"]];
+
+let length = bracketsConfig.length;
+let result = [];
+
+function iterator (bracketsConfig, callback) {
+    // let temp = null;
+
+    let item, index = 0;
+    length = bracketsConfig.length;
+
+    for (; index < length; index++) {
+        item = bracketsConfig[index];
+
+        if (Object.prototype.toString.call(item) === '[object Array]') {
+            iterator(item, callback);
+        } else {
+            callback(item);
+        }
+    }
+    console.log(result); //нужно проверять каждый вложенный, тут точка создания их (и обнулять массив после проверки
+    //---------->>>>>>>>>>>
+    checkArr(result);
+    //----------<<<<<<<<<<<
+
+    // if (result[0] === undefined) {
+    //     console.log("массив пустой");
+    //     result.length = 0;
+    //     return true;
+    // } else {
+    //     console.log("массив НЕ пустой");
+    //     return false
+    // }
+
+}
+
+// Будет вызвана для каждого элемента, не являющимся массивом.
+function callback (item) {
+    result.push(item);
+}
+
+iterator(bracketsConfig, callback);
+
+function checkArr(arr) {
+    let result = [];
+
+    if (arr[0] === ")"  //Проверим, что аргумент НЕ начинается с закрывающейся скобки
+        || arr[0] === "}"
+        || arr[0] === "]") {
+        console.log("Ошибка в скобках #1");
+        return false; //И первый выход с ошибкой из программы
+    }
+
+    result.push(arr[0]);
+
+    for (let i = 1; i < arr.length; i++){
+        let j = 0;
+
+        if (result.length >= 1 && result[j] !== arr[i]) { //если пара совпала, удаляем из стека
+            result.shift();
+        } else {
+            result.push(arr[i]); //если пара не совпала - добавляем в стек
+            j++;
+        }
+
+        console.log(result);
+        if (result[0] === undefined) {
+            console.log("НЕТ ошибок в скобках");
+            result.length = 0; //очищаем массив
+            console.log(result);
+            return true;
+        } else {
+            console.log("Ошибка в скобках №2");
+            result.length = 0; //очищаем массив
+            console.log(result);
+            return false
+        }
+    }
+
+}
 // console.log(check(str));
